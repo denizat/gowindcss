9802cb3c31395d455590b6bcaad320a9696e1cc6
Deniz Telci <deniza.telci@gmail.com>
2024-01-12 11:31:48 -0600

    added zinc

--- main.go
+++ main.go
@@ -1332,7 +1332,17 @@ var defaultColors = map[string]string{
 	"gray-900": "#111827",
 	"gray-950": "#030712",
 
-	// I forgot zinc
+	"zinc-50":  "#fafafa",
+	"zinc-100": "#f4f4f5",
+	"zinc-200": "#e4e4e7",
+	"zinc-300": "#d4d4d8",
+	"zinc-400": "#a1a1aa",
+	"zinc-500": "#71717a",
+	"zinc-600": "#52525b",
+	"zinc-700": "#3f3f46",
+	"zinc-800": "#27272a",
+	"zinc-900": "#18181b",
+	"zinc-950": "#09090b",
 
 	"neutral-50":  "#fafafa",
 	"neutral-100": "#f5f5f5",
